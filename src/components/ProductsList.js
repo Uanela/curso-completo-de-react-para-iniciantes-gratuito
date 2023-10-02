02ca3ba1b4c5c3c9cb6ce5d360c9a6a11a4cedd6
Uanela Como <104281989+Uanela@users.noreply.github.com>
2023-10-02 21:02:27 +0200

    Update ProductsList.js

--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -4,10 +4,11 @@ export default function ProductsList({ products, title }) {
   return (
     <div className="products-list">
       <h2>{title}</h2>
-      {products.map((produtct) => (
-        <div className="product-preview" key={produtct.id}>
-          <h2>{produtct.name}</h2>
-          <p>Adicionado por {produtct.worker}</p>
+      {products.map((product) => (
+        <div className="product-preview" key={product.id}>
+          <h2>{product.name}</h2>
+          <p>Adicionado por {product.worker}</p>
+         <button onClick={() => handleDelete(product.id)}>Apagar</button>
         </div>
       ))}
     </div>
