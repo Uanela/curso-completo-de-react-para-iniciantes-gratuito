a6535964362e52f6ed3064fa92f5884154b906df
uanela <uanelaluiswayne@gmail.com>
2023-10-02 22:10:39 +0200

    frist commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,48 +2,17 @@ import { useState, useEffect } from "react";
 import ProductsList from "./ProductsList";
 
 export default function Home() {
-  const [products, setProducts] = useState([
-    {
-      name: "Samsung S24 Ultra",
-      description: "producto teste adicionado",
-      worker: "Uanela",
-      id: 1,
-    },
-    {
-      name: "iPhone 15 Pro Max",
-      description: "producto teste adicionado",
-      worker: "Jobs",
-      id: 2,
-    },
-    {
-      name: "Xiaomi 13 Ultra",
-      description: "producto teste adicionado",
-      worker: "Uanela",
-      id: 3,
-    },
-  ]);
-
-  const [name, setName] = useState("Uanela");
-
-  const handleDelete = (id) => {
-    const newProdutcts = products.filter((product) => product.id !== id);
-    setProducts(newProdutcts);
-  };
+  const [products, setProducts] = useState(null);
 
   useEffect(() => {
-    console.log("useEffect executou");
-    console.log(name);
-  }, [name]);
+    fetch("http://localhost:8000/products")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
 
   return (
     <div className="home">
-      <ProductsList
-        products={products}
-        title="Todos Productos"
-        handleDelete={handleDelete}
-      />
-      <button onClick={() => setName("Como")}>Trocar nome</button>
-      <p>{name}</p>
+      {products && <ProductsList products={products} title="Todos Productos" />}
     </div>
   );
 }
--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function ProductsList({ products, title, handleDelete }) {
+export default function ProductsList({ products, title }) {
   return (
     <div className="products-list">
       <h2>{title}</h2>
@@ -8,7 +8,7 @@ export default function ProductsList({ products, title, handleDelete }) {
         <div className="product-preview" key={product.id}>
           <h2>{product.name}</h2>
           <p>Adicionado por {product.worker}</p>
-          <button onClick={() => handleDelete(product.id)}>Apagar</button>
+          <p>Preço: {product.price} Mzn</p>
         </div>
       ))}
     </div>
