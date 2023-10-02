865ead86fbc5ae5dd5b22eebf5758518605bacd3
uanela <uanelaluiswayne@gmail.com>
2023-10-02 13:55:56 +0200

    first commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,21 @@
 import React from "react";
 
 export default function Home() {
-  return <div>Homepage</div>;
+  const handleClick = (e) => {
+    console.log("Olá Uanela", e);
+  };
+
+  const handleClickAgain = (name, e) => {
+    console.log("Olá " + name, e.target);
+  };
+
+  return (
+    <div className="home">
+      <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={(e) => handleClickAgain("Uanela", e)}>
+        Click me again
+      </button>
+    </div>
+  );
 }
