6d0fb2e86af2e8b2f67098274d7dd4479f40279d
uanela <uanelaluiswayne@gmail.com>
2023-10-08 16:07:52 +0200

    first commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,20 +6,36 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export default function Home() {
   const [products, setProducts] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(null);
 
   useEffect(() => {
     setTimeout(() => {
-      fetch("http://localhost:8000/products")
-        .then((res) => res.json())
+      fetch("http://localhost:8000/productss")
+        .then((res) => {
+          if (!res.ok)
+            throw Error("Não foi possivel carregar os dados para este recurso");
+          return res.json();
+        })
         .then((data) => {
           setProducts(data);
           setIsLoading(false);
+          setIsError(null);
+        })
+        .catch((err) => {
+          setIsError(err.message);
+          setIsLoading(false);
+          setProducts(null);
         });
     }, 1000);
   }, []);
 
   return (
     <div className="home">
+      {isError && (
+        <p className="error-message">
+          <span>!</span> {isError}
+        </p>
+      )}
       {isLoading && (
         <div className="notch-container">
           <FontAwesomeIcon icon={faCircleNotch} className="loading-notch" />
