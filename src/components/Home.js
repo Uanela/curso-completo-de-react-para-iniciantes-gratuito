ad0c67cd5b11134fd88df61009bfcce1e0912df3
uanela <uanelaluiswayne@gmail.com>
2023-10-08 15:41:25 +0200

    first commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import ProductsList from "./ProductsList";
+import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Home() {
   const [products, setProducts] = useState(null);
@@ -18,7 +20,11 @@ export default function Home() {
 
   return (
     <div className="home">
-      {isLoading && <p>Carregando...</p>}
+      {isLoading && (
+        <div className="notch-container">
+          <FontAwesomeIcon icon={faCircleNotch} className="loading-notch" />
+        </div>
+      )}
       {products && <ProductsList products={products} title="Todos Productos" />}
     </div>
   );
