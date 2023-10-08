70c15f9fbdb31f063d89bc9a8dfeb77e4c9a349e
uanela <uanelaluiswayne@gmail.com>
2023-10-08 18:05:11 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <main>
-        <Home />
-      </main>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<p>criar novo blog</p>} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
