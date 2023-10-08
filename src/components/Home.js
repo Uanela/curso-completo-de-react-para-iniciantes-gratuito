8f395c596f335fff2e19354f74db34f510806735
uanela <uanelaluiswayne@gmail.com>
2023-10-08 15:26:32 +0200

    first commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,15 +3,22 @@ import ProductsList from "./ProductsList";
 
 export default function Home() {
   const [products, setProducts] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    fetch("http://localhost:8000/products")
-      .then((res) => res.json())
-      .then((data) => setProducts(data));
+    setTimeout(() => {
+      fetch("http://localhost:8000/products")
+        .then((res) => res.json())
+        .then((data) => {
+          setProducts(data);
+          setIsLoading(false);
+        });
+    }, 1000);
   }, []);
 
   return (
     <div className="home">
+      {isLoading && <p>Carregando...</p>}
       {products && <ProductsList products={products} title="Todos Productos" />}
     </div>
   );
